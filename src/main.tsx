00a059feb8b02392e5f10799b4d72eab5534e38b
imGuii <69533374+imGui33@users.noreply.github.com>
2022-09-30 15:22:31 -0400

    a
    a

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./components/contexts/auth";
+import { Rotas } from "./components/Routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthContextProvider>
+      <Rotas />
       <App />
     </AuthContextProvider>
   </React.StrictMode>
